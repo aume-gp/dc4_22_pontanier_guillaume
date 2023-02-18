369a87cdb4b148a46211ffec83bf8e2cd93d3ce2
aume-gp <77577837+aume-gp@users.noreply.github.com>
2023-02-18 11:29:49 +0100

    Suppression dossier mauvais nom

--- exo06/ft_print_comb2.js
+++
@@ -1,9 +0,0 @@
-function ft_print_comb2() {
-    for (let a = 0; a<100; a++) {
-        for (let b = a + 1; b<100; b++) {
-            console.log(String(a).padStart(2, '0') + ' ' + String(b).padStart(2, '0'));
-        }
-    }
-}
-
-ft_print_comb2();
